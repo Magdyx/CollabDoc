a15499d097278ada7e33bba2d9e8375f013dc7f3
Magdy <elansarymaster3@yahoo.com>
2021-01-27 13:47:56 +0200

    add the ability to add and remove editors from the document view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :documents
+  resources :documents do
+    resources :documents_users, only: [:new, :create, :destroy, :index]
+  end
   resources :operations, only: :create
   root "documents#index"
 end
